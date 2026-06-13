162f28028d55485629e7732ebc13b6c8ef5b0ac4
Edbbioeco <edsonbbiologia@gmail.com>
2026-06-13 17:28:49 -0300

    Lista de dados espectrais

--- gerar_evalscripts.R
+++ gerar_evalscripts.R
@@ -5,3 +5,13 @@ library(rsi)
 library(tidyverse)
 
 library(CDSE)
+
+# Gerar evalscripts ----
+
+## Lista de dados espectrais ----
+
+dados_espectrais <- rsi::spectral_indices() |>
+  dplyr::pull(short_name) |>
+  unique()
+
+dados_espectrais
