3281531b3cec9653f27c6c8753c353a7044df555
Edbbioeco <edsonbbiologia@gmail.com>
2026-06-13 17:50:50 -0300

    Criar evalscripts

--- gerar_evalscripts.R
+++ gerar_evalscripts.R
@@ -21,3 +21,35 @@ dados_espectrais
 colecoes <- c("sentinel-1", "sentinel-2", "landsat")
 
 colecoes
+
+## Gerar evalscripts ----
+
+evalscripts <- purrr::map(
+  dados_espectrais,
+  purrr::in_parallel(
+
+    \(espec){
+
+      purrr::map(colecoes,
+                 \(colecoes){
+
+                   tryCatch(
+
+                     rsi::spectral_indices() |>
+                       dplyr::filter(short_name == espec) |>
+                       CDSE::MakeEvalScript(constellation = colecoes),
+                     error = \(e) NULL
+
+                   )
+
+                   }
+
+                 )
+
+      }
+
+    ),
+  .progress = TRUE) |>
+  purrr::list_flatten()
+
+evalscripts
